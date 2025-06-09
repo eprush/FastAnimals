7ee6c1e9c239b43d7184d654a4dba21f5214c558
Egor Pavlovich <pavlovich.er@phystech.edu>
2025-06-10 01:17:39 +0300

    use mapped_column instead of Column

--- app/models/animal.py
+++ app/models/animal.py
@@ -3,8 +3,9 @@ A module describing animal model with ORM
 """
 
 import uuid
-from sqlalchemy import Integer, Column, DateTime, String, func
-from sqlalchemy.dialects.postgresql import UUID
+from datetime import datetime
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column
 
 from app.models.base import BaseModel
 
@@ -14,10 +15,10 @@ class Animal(BaseModel):
 
     __tablename__ = "animal"
 
-    id = Column(Integer, primary_key=True)
-    animal_type = Column(String, nullable=False)
-    processed_image = Column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, nullable=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    id: Mapped[int] = mapped_column(primary_key=True)
+    animal_type: Mapped[str] = mapped_column(nullable=False)
+    processed_image: Mapped[uuid.UUID] = mapped_column(default=uuid.uuid4, unique=True)
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
 
     def __repr__(self):
         return (f"Animal with ( \nid={self.id}, \nanimal_type={self.animal_type},"
