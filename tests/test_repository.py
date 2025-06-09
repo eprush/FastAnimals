0b712b711309a17811e3a342b86342a8acae3a3f
Egor Pavlovich <pavlovich.er@phystech.edu>
2025-06-10 01:17:12 +0300

    create test group TestAnimalRepository

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -1,7 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from collections.abc import AsyncGenerator
 from typing import Iterable
-import asyncio
 import uuid
 import pytest_asyncio
 import pytest
@@ -9,6 +8,7 @@ import pytest
 from app.core.config import Settings, get_settings_no_cache
 from app.repositories.animals import SQLAlchemyAnimalRepository
 from app.models.animal import Animal
+from app.models.base import BaseModel
 
 app_settings: Settings = get_settings_no_cache()
 
@@ -25,29 +25,40 @@ async_session = async_sessionmaker(
     autoflush=False,
 )
 
-@pytest_asyncio.fixture(scope="module")
+@pytest.fixture(scope="session", autouse=True)
+def setup_db():
+    BaseModel.metadata.drop_all(async_engine)
+    BaseModel.metadata.create_all(async_engine)
+    yield
+    BaseModel.metadata.drop_all(async_engine)
+
+
+@pytest_asyncio.fixture(scope="class")
 async def session() -> AsyncGenerator[AsyncSession]:
     """Fixture for creating a new test session."""
     async with async_session() as s:
         yield s
 
-@pytest.fixture(scope="module")
+
+@pytest.fixture(scope="class")
 def repo(session):
     return SQLAlchemyAnimalRepository(db_session=session)
 
 
-@pytest.mark.asyncio(loop_scope="module")
-async def test_can_create_any_animal(repo):
-    animal = await repo.add_by_name("capybara")
-    print(animal)
-    assert type(animal) is Animal
+class TestAnimalRepository:
+    @pytest.mark.parametrize("name", ("capybara", "giraffe", "wolf"))
+    @pytest.mark.asyncio(loop_scope="class")
+    async def test_can_create_any_animal(self, repo, name):
+        animal = await repo.add_by_name(name)
+        print(animal)
+        assert type(animal) is Animal
 
-@pytest.mark.asyncio(loop_scope="module")
-async def test_cannot_get_unexistent(repo):
-    animal = await repo.get_by_uuid(uuid.uuid4())
-    assert animal is None
+    @pytest.mark.asyncio(loop_scope="class")
+    async def test_cannot_get_unexistent(self, repo):
+        animal = await repo.get_by_uuid(uuid.uuid4())
+        assert animal is None
 
-@pytest.mark.asyncio(loop_scope="module")
-async def test_get_iterable_animals(repo):
-    animals = await repo.get_all()
-    assert isinstance(animals, Iterable)
+    @pytest.mark.asyncio(loop_scope="class")
+    async def test_get_iterable_animals(self, repo):
+        animals = await repo.get_all()
+        assert isinstance(animals, Iterable)
