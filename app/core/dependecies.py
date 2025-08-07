54b0c070b35c49b5dbb51593bb5285bad8f4dbfc
Egor Pavlovich <pavlovich.er@phystech.edu>
2025-08-07 15:13:30 +0300

    delete unnecessary asyncio.sleep command

--- app/core/dependecies.py
+++ app/core/dependecies.py
@@ -2,16 +2,14 @@
 The module that defines the dependencies.
 Among them, connection to the database, dependence on their own services
 """
-import asyncio
 from collections.abc import AsyncGenerator
 from typing import Annotated
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from fastapi import Depends
-from asyncio import sleep
 
 from app.core.config import Settings, get_app_settings
 from app.services.animal import AnimalService
-from app.services.image import AnimalImage
+from app.services.image import AnimalImageService
 from app.services.email import EmailService
 
 app_settings: Settings = get_app_settings()
@@ -43,15 +41,14 @@ DatabaseDependence = Annotated[AsyncSession, Depends(get_db)]
 
 async def get_animal_service(db: DatabaseDependence) -> AnimalService:
     """Returns an instance of AnimalsService."""
-    await sleep(1)
     return AnimalService(db_session=db)
 
-async def get_image_service() -> AnimalImage:
-    await sleep(1)
-    return AnimalImage()
+async def get_image_service() -> AnimalImageService:
+    """Returns an instance of AnimalImageService."""
+    return AnimalImageService()
 
 async def get_email_service() -> EmailService:
-    await asyncio.sleep(1)
+    """Returns an instance of EmailService."""
     return EmailService(
         to_email=app_settings.email_address,
         from_email=app_settings.email_address,
@@ -59,5 +56,5 @@ async def get_email_service() -> EmailService:
     )
 
 AnimalServiceDependence = Annotated[AnimalService, Depends(get_animal_service)]
-ImageServiceDependence = Annotated[AnimalImage, Depends(get_image_service)]
+ImageServiceDependence = Annotated[AnimalImageService, Depends(get_image_service)]
 EmailServiceDependence = Annotated[EmailService, Depends(get_email_service)]
--- app/services/image.py
+++ app/services/image.py
@@ -11,7 +11,7 @@ from app.core.config import (
 )
 
 
-class AnimalImage:
+class AnimalImageService:
     @staticmethod
     def get_image_name(name: UUID) -> str:
         """ A method for getting the name of image. """
@@ -20,12 +20,12 @@ class AnimalImage:
     @staticmethod
     def get_image_path(name: UUID) -> str:
         """ A method for getting the path of image. """
-        return os.path.join(get_static_dir(), AnimalImage.get_image_name(name))
+        return os.path.join(get_static_dir(), AnimalImageService.get_image_name(name))
 
     @staticmethod
     def save_image(data: bytes, *, name: UUID) -> str:
         """ A method for saving the image. """
-        image_path = AnimalImage.get_image_path(name)
+        image_path = AnimalImageService.get_image_path(name)
         #image = Image.frombytes("L", (3, 2), data) # how to get image sizes tuple
         #image.save(filepath)
         with open(image_path, "wb") as file:
