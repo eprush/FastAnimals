7e57c163662062e640194a379301643b651cee15
Egor Pavlovich <pavlovich.er@phystech.edu>
2025-08-09 19:04:49 +0300

    refactor

--- app/services/email.py
+++ app/services/email.py
@@ -6,30 +6,30 @@ from app.schemas.email import EmailSchema
 
 
 class EmailService:
-    def __init__(self, to_email, from_email, password):
-        self._email: EmailSchema = EmailSchema(
+    def __init__(self, to_email, from_email, password: str) -> None:
+        self._email_info: EmailSchema = EmailSchema(
             to_email=to_email,
             from_email=from_email,
             password=password,
         )
 
     def send_email(self, message: str) -> None:
-        self._email.body = message
+        self._email_info.body = message
         try:
             # Создаем сообщение
             msg = MIMEMultipart()
-            msg['From'] = self._email.from_email
-            msg['To'] = self._email.to_email
-            msg['Subject'] = self._email.subject
+            msg['From'] = self._email_info.from_email
+            msg['To'] = self._email_info.to_email
+            msg['Subject'] = self._email_info.subject
 
             # Добавляем текст сообщения
-            msg.attach(MIMEText(self._email.body, 'plain'))
+            msg.attach(MIMEText(self._email_info.body, 'plain'))
 
             # Подключаемся к SMTP серверу и отправляем письмо
-            server = smtplib.SMTP(self._email.smtp_server, self._email.port)
+            server = smtplib.SMTP(self._email_info.smtp_server, self._email_info.port)
             server.starttls()  # Защищенное соединение
-            server.login(self._email.from_email, self._email.password)
-            server.sendmail(self._email.from_email, self._email.to_email, msg.as_string())
+            server.login(self._email_info.from_email, self._email_info.password)
+            server.sendmail(self._email_info.from_email, self._email_info.to_email, msg.as_string())
             server.quit()
 
             print("Письмо успешно отправлено!")
